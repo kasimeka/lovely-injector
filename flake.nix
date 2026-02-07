c584b18f7306c9478ccbf0686cf8f73f98b34867
ورد <kasimeka@tuta.io>
2026-02-07 17:29:32 +0200

    nix flake: drop flake-utils boilerplate, compile crate with naersk

--- flake.nix
+++ flake.nix
@@ -2,12 +2,17 @@
   nixConfig.bash-prompt-prefix = ''\[\e[0;31m\](lovely) \e[0m'';
 
   inputs = {
-    # requires nix `>=v2.27` or determinate-nix `v3`, will be released in lix `v2.94`
+    # requires nix `>=v2.27`, determinate-nix `v3`, or lix `>=v2.94`
     self.submodules = true;
 
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
@@ -20,81 +25,75 @@
     };
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import inputs.nixpkgs {
+  outputs = inputs: let
+    forAllSystems = f:
+      inputs.nixpkgs.lib.genAttrs
+      (import inputs.systems)
+      (system:
+        f (import inputs.nixpkgs {
           inherit system;
           overlays = [inputs.rust-overlay.overlays.default];
+        })
+        system);
+  in {
+    packages = forAllSystems (pkgs: _: let
+      rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      naersk = pkgs.callPackage inputs.naersk {
+        cargo = rust-toolchain;
+        rustc = rust-toolchain;
+      };
+
+      pname = (pkgs.lib.importTOML ./crates/lovely-unix/Cargo.toml).package.name;
+      version = (pkgs.lib.importTOML ./crates/lovely-core/Cargo.toml).package.version;
+      src = pkgs.lib.cleanSourceWith {
+        name = "${pname}-${version}-clean-src";
+        src = ./.;
+        filter = inputs.gitignore.lib.gitignoreFilterWith {
+          basePath = ./.;
+          extraRules =
+            # gitignore
+            ''
+              flake.*
+              LICENSE.md
+              README.md
+              .github
+            '';
         };
-        rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        rust-platform = pkgs.makeRustPlatform {
-          cargo = rust-toolchain;
-          rustc = rust-toolchain;
-        };
+      };
 
-        pname = (pkgs.lib.importTOML ./crates/lovely-unix/Cargo.toml).package.name;
-        version = (pkgs.lib.importTOML ./crates/lovely-core/Cargo.toml).package.version;
-        src = pkgs.lib.cleanSourceWith {
-          name = "${pname}-${version}-clean-src";
-          src = ./.;
-          filter = inputs.gitignore.lib.gitignoreFilterWith {
-            basePath = ./.;
-            extraRules =
-              # gitignore
-              ''
-                flake.*
-                LICENSE.md
-                README.md
-                .github
-              '';
-          };
-        };
-        drv = rust-platform.buildRustPackage {
-          inherit src pname version;
+      drv = naersk.buildPackage {
+        inherit src pname version;
+        cargoBuildOptions = x: x ++ ["--package" "lovely-unix"];
+        copyLibs = true;
+        nativeBuildInputs = with pkgs; [cmake];
+      };
+    in {
+      # `nix build git+https://github.com/ethangreen-dev/lovely-injector && ls result/lib`
+      default = drv;
+      lovely-injector = drv;
+    });
 
-          doCheck = false;
+    devShells = forAllSystems (pkgs: system: let
+      rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    in {
+      # `nix develop git+https://github.com/ethangreen-dev/lovely-injector`
+      default = pkgs.mkShell {
+        # grab all build dependencies of all exposed packages
+        inputsFrom = pkgs.lib.attrValues inputs.self.packages.${system};
+        shellHook = ''echo "with löve from wrd :)"'';
+      };
 
-          useFetchCargoVendor = true;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes."retour-0.4.0-alpha.2" = "sha256-GtLTjErXJIYXQaOFLfMgXb8N+oyHNXGTBD0UeyvbjrA=";
-          };
-          cargoBuildFlags = ["--package" "lovely-unix"];
-
-          nativeBuildInputs = with pkgs; [cmake];
-
-          env.RUST_BACKTRACE = 1;
-        };
-      in {
-        packages = {
-          # `nix build git+https://github.com/ethangreen-dev/lovely-injector && ls result/lib`
-          default = inputs.self.packages.${system}.lovely-injector;
-          lovely-injector = drv;
-        };
-
-        devShells = {
-          # `nix develop git+https://github.com/ethangreen-dev/lovely-injector`
-          default = inputs.self.devShells.${system}.base;
-
-          base = pkgs.mkShell {
-            # grab all build dependencies of all exposed packages
-            inputsFrom = pkgs.lib.attrValues inputs.self.packages.${system};
-            shellHook = ''echo "with löve from wrd :)"'';
-          };
-
-          # `nix develop git+https://github.com/ethangreen-dev/lovely-injector#full`
-          full = pkgs.mkShell {
-            # inherit the base shell
-            inputsFrom = [inputs.self.devShells.${system}.base];
-            packages =
-              (with pkgs; [luajit love])
-              ++ [
-                (rust-toolchain.override
-                  {extensions = ["rust-src" "rust-analyzer"];})
-              ];
-          };
-        };
-      }
-    );
+      # `nix develop git+https://github.com/ethangreen-dev/lovely-injector#full`
+      full = pkgs.mkShell {
+        # inherit the base shell
+        inputsFrom = [inputs.self.devShells.${system}.default];
+        packages =
+          (with pkgs; [luajit love])
+          ++ [
+            (rust-toolchain.override
+              {extensions = ["rust-src" "rust-analyzer"];})
+          ];
+      };
+    });
+  };
 }
